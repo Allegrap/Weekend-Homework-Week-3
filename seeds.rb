dd3f0000ec1793d096efd6da3250eb85d4bac1cd
Allegra Perazzo <allegra.perazzo@googlemail.com>
2017-03-24 17:10:21 +0000

    new instances added

--- seeds.rb
+++ seeds.rb
@@ -13,15 +13,41 @@ customer1 = Customer.new({
   })
 customer1.save()
 
+customer2 = Customer.new({
+  'name' => 'Chiara',
+  'funds' => 13
+  })
+customer2.save()
+
+
 film1 = Film.new({
   'title' => 'sing street',
   'price' => 15
   })
 film1.save()
 
+film2 = Film.new({
+  'title' => 'pride',
+  'price' => 25
+  })
+film1.save()
+
+
 ticket1 = Ticket.new({
   'customer_id' => customer1.id,
   'film_id' => film1.id
+  })
+ticket1.save()
+
+ticket2 = Ticket.new({
+  'customer_id' => customer2.id,
+  'film_id' => film1.id
+  })
+ticket1.save()
+
+ticket3 = Ticket.new({
+  'customer_id' => customer1.id,
+  'film_id' => film2.id
   })
 ticket1.save()
 
